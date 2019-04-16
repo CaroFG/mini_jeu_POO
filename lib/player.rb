e2fbbd84d3057361c74dc8fcc330111026511c2f
Jahlawa <belabbes.l@hotmail.fr>
2019-04-16 15:17:03 +0200

    V2 en cours

--- lib/player.rb
+++ lib/player.rb
@@ -45,13 +45,50 @@ class Player
 
 end
 
-
+# HumanPlay hérite des propriétés de Player
 class HumanPlayer < Player
+	# On ajoute l'attribut weapon_level
 	attr_accessor :weapon_level
 
+	# On définit les changements, le nom fonctionne comme pour Player,
+	# mais cette fois on a une arme weapon_level de niveau 1,
+	# et le joueur humain commence avec 100 point de vie.
 	def initialize(name)
 		super(name)
 		@weapon_level = 1
 		@life_points = 100	
+	end
+
+
+	def show_state
+		# On modifie le show_state pour ajouter le niveau de l'arme
+		return puts "#{@name} a #{@life_points} points de vie et possède une arme de niveau #{@weapon_level}." 
 	end	
+
+	def compute_damage
+		# On multiplie le niveau de l'arme pour avoir un bonus de dégats
+		rand(1..6) * @weapon_level
+	end
+
+	# Méthode pour chercher une arme et augmenter le bonus de dégats
+	def search_weapon
+		# On trouve au hasard une arme de niveau 1 à 6
+		weapon_found = rand(1..6)
+		puts "Bravo, tu as trouvé une arme de niveau #{weapon_found}."
+
+		# Si l'arme trouvée est d'un niveau supérieur à celle qu'on à, on la garde
+		# et applique son bonus de dégats
+		if weapon_found > @weapon_level
+			@weapon_level = weapon_found
+			puts "Yeah, cette nouvelle arme est meilleure, je la garde"
+		else
+			puts "Mince, cette arme est pourrie, je ne la garde pas"
+		end 
+	end
+
+	def search_health_pack
+		rand(1..6)
+		
+	end
+
 end
