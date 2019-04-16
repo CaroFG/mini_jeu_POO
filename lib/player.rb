b3718effd5df4a11c9291e3dea0645c5dfd336c2
CaroFG <carolina.ferreira131@gmail.com>
2019-04-16 12:37:03 +0200

    second commit

--- lib/player.rb
+++ lib/player.rb
@@ -13,28 +13,30 @@ class Player
 	end
 
 	# Methode pour les points de dégat
-	def gets_damage(dmg)
-		
-		puts "#{@name} subit #{dmg} points de dégats"
+	def gets_damage(damage)
+		@dmg = compute_damage.to_i
 
 		# On soustrait les dégats aux points de vie
-		@life_points -= dmg
+		@life_points -= @dmg
 
 		# Si les points de vie sont inférieurs ou égal à 0, le personnage est mort
 		# et on prévient l'utilisateur
 		if @life_points	<= 0 
 			puts "#{@name} est mort."
 		end	
-	end
 
-	# Méthode pour obtenir un nombre de dégats au hasard
-	def compute_damage
-		return rand(1..6)
 	end
 
 	# Méthode pour qu'un joueur en attaque un autre
 	def attacks(player2)
 		puts "#{@name} attaque #{player2.name}"
+		player2.gets_damage
+		puts "#{@name} inflige #{@dmg} points de dégats à #{player2.name}"
+	end
+
+		# Méthode pour obtenir un nombre de dégats au hasard
+	def compute_damage
+		return rand(1..6)
 	end
 
 end
